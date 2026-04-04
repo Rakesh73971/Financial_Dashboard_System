c2f17a2f2b32cf5bbc43ef2a9145b5b97f30d6a2
Rakesh N <nraki522@gmail.com>
2026-04-04 12:37:52 +0530

    fixed errors

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, Query,status,Request
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.schemas.transaction import *
-from app.services.record_service import *
+from app.services.transaction_service import *
 from app.core.permissions import require_role
 from app.core.rate_limiter import limiter
 
