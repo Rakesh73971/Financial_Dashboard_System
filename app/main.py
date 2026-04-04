a642ab0adaf25744c29c994fdf41ea466fc3a3e9
Rakesh N <nraki522@gmail.com>
2026-04-04 11:10:03 +0530

    added the rate limiter

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 from app.db.session import engine, Base
-from app.routes import auth,records,dashboard
+from app.routes import auth,transactions,dashboard
+from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
-from app.core.limiter import limiter
+from app.core.rate_limiter import limiter
 
 
 
 app = FastAPI()
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
+app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
-app.include_router(records.router)
+app.include_router(transactions.router)
 app.include_router(dashboard.router)
