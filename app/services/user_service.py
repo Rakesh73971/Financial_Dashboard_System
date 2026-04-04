cf17365d139df8c28b2c3f29c41a42a21a2a4644
Rakesh N <nraki522@gmail.com>
2026-04-04 12:45:56 +0530

    added the user services

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.core.security import hash_password, verify_password
+from app.schemas.user import UserUpdate
+
+
 
 def create_user(db: Session, user):
     db_user = User(
@@ -14,14 +17,34 @@ def create_user(db: Session, user):
     db.refresh(db_user)
     return db_user
 
-
 def authenticate_user(db: Session, email: str, password: str):
     user = db.query(User).filter(User.email == email).first()
-
-    if not user:
+    if not user or not user.is_active: # Check if user is active!
         return None
-
     if not verify_password(password, user.password):
         return None
-
     return user
+
+
+
+def get_all_users(db: Session):
+    
+    return db.query(User).all()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def update_user(db: Session, user_id: int, updates: UserUpdate):
+   
+    db_user = get_user_by_id(db, user_id)
+    if not db_user:
+        return None
+
+   
+    update_data = updates.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_user, key, value)
+
+    db.commit()
+    db.refresh(db_user)
+    return db_user
