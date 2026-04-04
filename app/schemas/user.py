450cf332b595405d4799c3849eab283bde031d05
Rakesh N <nraki522@gmail.com>
2026-04-04 12:38:48 +0530

    added the user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr, ConfigDict
+from typing import Optional
 
 class UserCreate(BaseModel):
     username: str
@@ -14,3 +15,11 @@ class UserResponse(BaseModel):
     is_active: bool
 
     model_config = ConfigDict(from_attributes=True)
+
+
+
+
+class UserUpdate(BaseModel):
+    role: Optional[str] = None
+    is_active: Optional[bool] = None
+    username: Optional[str] = None
