83dbaa2ad6adb7a993b04ec06e3df389fc0d8535
Rakesh N <nraki522@gmail.com>
2026-04-04 12:46:34 +0530

    added the status code

--- app/routes/users.py
+++ app/routes/users.py
@@ -3,42 +3,39 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.db.session import get_db
 from app.schemas.user import UserResponse, UserUpdate
-from app.models.user import User
 from app.core.permissions import require_role
+from app.services.user_service import get_all_users, update_user 
 
 router = APIRouter(prefix="/users", tags=["User Management"])
 
-@router.get("/", response_model=List[UserResponse])
-def get_all_users(
+@router.get("/", status_code=status.HTTP_200_OK, response_model=List[UserResponse])
+def read_all_users(
     db: Session = Depends(get_db),
     current_user=Depends(require_role(["admin"]))
 ):
+    return get_all_users(db)
 
-    return db.query(User).all()
-
-@router.get("/me", response_model=UserResponse)
+@router.get("/me", status_code=status.HTTP_200_OK, response_model=UserResponse)
 def get_my_profile(
     current_user=Depends(require_role(["viewer", "analyst", "admin"]))
 ):
-
+    
     return current_user
 
-@router.patch("/{user_id}", response_model=UserResponse)
-def update_user_status_or_role(
+@router.patch("/{user_id}", status_code=status.HTTP_202_ACCEPTED, response_model=UserResponse)
+def update_user_access(
     user_id: int,
     updates: UserUpdate,
     db: Session = Depends(get_db),
     current_user=Depends(require_role(["admin"]))
 ):
-    db_user = db.query(User).filter(User.id == user_id).first()
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
+   
+    updated_user = update_user(db, user_id=user_id, updates=updates)
     
-
-    update_data = updates.model_dump(exclude_unset=True)
-    for key, value in update_data.items():
-        setattr(db_user, key, value)
+    if not updated_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="User not found"
+        )
     
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    return updated_user
