3894f3f790968f923187361601a62e487fd2b304
Rakesh N <nraki522@gmail.com>
2026-04-05 09:31:11 +0530

    added enum default

--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,21 @@
-from sqlalchemy import Column,Integer,String,Boolean
+import enum
+from sqlalchemy import Column, Integer, String, Boolean, Enum
 from app.db.session import Base
 from sqlalchemy.orm import relationship
+
+
+class UserRole(str, enum.Enum):
+    admin = "admin"
+    analyst = "analyst"
+    viewer = "viewer"
 
 class User(Base):
     __tablename__= "users"
 
-    id = Column(Integer,primary_key=True,index=True)
-    username = Column(String,nullable=False)
-    email = Column(String,unique=True,nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     password = Column(String)
-    role = Column(String,default='viewer')
-    is_active = Column(Boolean,default=True)
-    
+    role = Column(Enum(UserRole), default=UserRole.viewer, nullable=False)
+    is_active = Column(Boolean, default=True)
     transactions = relationship("Transaction", back_populates="owner")
-
-
